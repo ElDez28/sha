c6d977ee30024a3371c76fec395cbdea07f09ca3
Eldin Dezmic <eldin.dezmic@tech-towers.net>
2024-02-04 22:14:13 +0100

    images fix

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -20,11 +20,37 @@ const HeroSection = (props) => {
 
   return (
     <section
-      className={`relative flex-1 min-h-[60vh] flex items-center justify-center ${
+      className={`relative flex-1 min-h-[60vh]  flex items-center justify-center ${
         props.main ? "" : "md:max-h-[60vh]"
       }`}
     >
-      <div className="absolute left-0 top-0 -z-30 h-full w-full flex items-center justify-center"></div>
+      <div className="absolute left-0 top-0 -z-30 h-full w-full flex items-center justify-center">
+        <Image
+          alt="Tech background"
+          onLoad={onLoad}
+          className={`duration-600 hidden h-full w-full object-cover transition-all md:inline-block  ${
+            state[props.page] ? "" : "opacity-0"
+          }`}
+          src={props.bg}
+        ></Image>
+        <Image
+          alt="Tech background"
+          onLoad={onLoad}
+          className={`duration-600 h-full w-full object-cover transition-all md:hidden  ${
+            state[props.page] ? "" : "opacity-0"
+          }`}
+          src={props.bgMobile}
+        ></Image>
+      </div>
+      <div className="absolute left-0 top-0 -z-10 h-full w-full">
+        <Image
+          alt="Blurry background"
+          className={`duration-600 h-full w-full object-cover transition-all ${
+            state[props.page] ? "opacity-0" : ""
+          }`}
+          src={props.bg2}
+        ></Image>
+      </div>
       <div
         className={`duration-600 absolute left-0  top-0 -z-10 h-full w-full backdrop-blur-xl transition-all ${
           state[props.page] ? "opacity-0" : ""
